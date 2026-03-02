b2fc6ea9d9862f8fd095d23b7ba537709ff4134f
olga <koa0308@gmail.com>
2026-03-02 22:20:09 +0400

    корректировка flake8

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from config import settings
-from materials.models import Course, Lesson
 
 
 class User(AbstractUser):
